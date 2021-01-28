a6255c579ab9b27242985098113655757a82b2e5
BEN BRAHIM Mohamed El Amine <59963935+bbm213@users.noreply.github.com>
2021-01-28 15:44:21 +0100

    Add files via upload

---
+++ DAL/DataAccessLayer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Products_Management.DAL
+{
+    class DataAccessLayer
+    {
+        SqlConnection sqlconnection;
+
+        //This Constructor inisialize this object
+        public DataAccessLayer()
+        {
+            sqlconnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Product_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        //Method to Open the connection
+        public void Open() {
+            if (sqlconnection.State != ConnectionState.Open)
+            {
+                sqlconnection.Open();
+            }
+        }
+
+        //Method to close
+        public void Close()
+        {
+            if (sqlconnection.State == ConnectionState.Open)
+            {
+                sqlconnection.Close();
+            }
+        }
+
+        //Method to read data form database
+        public DataTable SelectData(string stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_procedure;
+            sqlcmd.Connection = sqlconnection;
+            if (param != null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                {
+                    sqlcmd.Parameters.Add(param[i]);
+                }
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        //Method to Insert, Update and Delete data from Database
+        public void ExecuteCommand(string stored_precedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_precedure;
+            sqlcmd.Connection = sqlconnection;
+            if (param != null)
+            {
+                sqlcmd.Parameters.AddRange(param);
+            }    
+            sqlcmd.ExecuteNonQuery();
+            
+        }
+
+
+    }
+}
